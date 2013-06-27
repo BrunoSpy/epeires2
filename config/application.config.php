e09792d7534d8d771db1047049b37575fdfb3449
BrunoSpy <bruno@spyckerelle.fr>
2013-06-27 21:29:31 +0200

    Correction config

--- config/application.config.php
+++ config/application.config.php
@@ -5,8 +5,7 @@ return array(
     	'ZendDeveloperTools',
     	'DoctrineModule',
     	'DoctrineORMModule',
-    	'Application',
-    	'HelloWorld'
+    	'Application'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
