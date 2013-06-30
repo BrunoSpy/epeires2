ef3909db34840fa16554f4d96c2d8b1a4d077378
BrunoSpy <bruno@spyckerelle.fr>
2013-06-30 21:13:12 +0200

    Correction route

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -8,13 +8,14 @@
 return array(
     'router' => array(
         'routes' => array(
-            'application' => array(
+        	'application' => array(
                 'type'    => 'segment',
+            	'may_terminate' => true,
                 'options' => array(
                     'route'    => '/[:controller[/:action]]',
                 	'constraints' => array(
                 			'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                    		'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                    		'controller' => '[a-zA-Z][a-zA-Z0-9-]*',
                		),
                     'defaults' => array(
                         '__NAMESPACE__' => 'Application\Controller',
