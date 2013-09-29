b048697dc3e1fa8d6305d66d3fc72ba4a48d5b3e
BrunoSpy <bruno@spyckerelle.fr>
2013-09-29 21:52:55 +0200

    correction bug

--- config/autoload/doctrine.local.php
+++ config/autoload/doctrine.local.php
@@ -8,7 +8,7 @@ return array(
 					'host'     => '127.0.0.1',
 					'port'     => '3306',
 					'user'     => 'root',
-					'password' => 'nqnspcla',
+					'password' => '',
 					'dbname'   => 'epeires2',
 					'charset' => 'utf8'
 )))));
