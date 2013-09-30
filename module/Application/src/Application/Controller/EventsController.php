46afd708e6cabccd2aeaccc3783c95246a3beadb
Bruno Spyckerelle <bruno@spyckerelle.fr>
2013-09-30 09:32:48 +0200

    encore un bug

--- module/Application/src/Application/Controller/EventsController.php
+++ module/Application/src/Application/Controller/EventsController.php
@@ -381,7 +381,7 @@ class EventsController extends AbstractActionController implements LoggerAware
     	$event = $objectManager->getRepository('Application\Entity\Event')->find($evtId);
     	
     	if($event){
-    		$event->setStatus($objectManager->getRepository('Application\Entity\Status')->findOneBy(array('default'=>true, 
+    		$event->setStatus($objectManager->getRepository('Application\Entity\Status')->findOneBy(array('defaut'=>true, 
     																									'open' => !$event->getStatus()->isOpen())));
     		$objectManager->persist($event);
     		$objectManager->flush();
