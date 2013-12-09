907e11bd20005564b4825710c8cfcc1a3b7bcd57
BrunoSpy <bruno@spyckerelle.fr>
2013-12-09 22:49:11 +0100

    Bug form.js causant décalage de ligne

--- module/Application/public/js/form.js
+++ module/Application/public/js/form.js
@@ -213,7 +213,8 @@ var form = function(url){
 	        success: function(data){
 				//close form
 				$("#create-link").trigger("click");
-				var id = $("#Event").find('input#id').val();
+				var id = $("#Event").find('input[name="id"]').val();
+				alert(id);
 				if(id>0){
 					//modification
 					timeline.modify(data.events, 0);
