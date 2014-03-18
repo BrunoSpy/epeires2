047c278dcf7ab6079636b4b2ffde27df032e4de9
Bruno Spyckerelle <bruno.spyckerelle@aviation-civile.gouv.fr>
2014-03-18 15:39:23 +0100

    Divers changements

---
+++ config/autoload/doctrine.production.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	'doctrine' => array(
+                 'configuration' => array(
+                        'orm_default' => array(
+                            'generate_proxies' => false,
+                        ),
+                 )
+        )
+);
+
