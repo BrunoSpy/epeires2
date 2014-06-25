795ce125fb43ad08bdb4390a6f1856649db85296
Bruno Spyckerelle <bruno.spyckerelle@aviation-civile.gouv.fr>
2014-06-25 11:53:03 +0200

    Oubli...

---
+++ data/DoctrineORMModule/Migrations/Version20140624143444.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace DoctrineORMModule\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+class Version20140624143444 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE customfields ADD tooltip LONGTEXT NOT NULL");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+        $this->addSql("ALTER TABLE customfields DROP tooltip");
+    }
+}
