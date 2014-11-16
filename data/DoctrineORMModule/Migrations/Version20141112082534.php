83d91ae439ea3e03a8f3755be37bf5b086b3094b
BrunoSpy <bruno@spyckerelle.fr>
2014-11-16 14:31:33 +0100

    Correction migration

--- data/DoctrineORMModule/Migrations/Version20141112082534.php
+++ data/DoctrineORMModule/Migrations/Version20141112082534.php
@@ -15,7 +15,7 @@ class Version20141112082534 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
         
-        $this->addSql('ALTER TABLE milcategory ADD filter VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE MilCategory ADD filter VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema)
