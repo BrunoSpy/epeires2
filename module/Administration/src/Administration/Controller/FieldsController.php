a0cbdcef60397bfa19b5cdd41678c8538c6d17a5
Bruno Spyckerelle <bruno.spyckerelle@aviation-civile.gouv.fr>
2014-12-02 15:30:36 +0100

    Début cohérence des champs en cas de changement

--- module/Administration/src/Administration/Controller/FieldsController.php
+++ module/Administration/src/Administration/Controller/FieldsController.php
@@ -1,46 +1,46 @@
 <?php
-/**
- * Epeires 2
- * @license   https://www.gnu.org/licenses/agpl-3.0.html Affero Gnu Public License
+/*
+ *  This file is part of Epeires².
+ *  Epeires² is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  Epeires² is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with Epeires².  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 namespace Administration\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use Zend\View\Model\JsonModel;
+
+use Application\Controller\FormController;
 use Application\Entity\CustomField;
+
 use Zend\Form\Annotation\AnnotationBuilder;
 use DoctrineModule\Stdlib\Hydrator\DoctrineObject;
 
-class FieldsController extends AbstractActionController
-{
-    public function indexAction()
-    {
-   	
-        return array();
-    }
-    
+/**
+ * 
+ * @license https://www.gnu.org/licenses/agpl-3.0.html Affero Gnu Public License
+ * @author Bruno Spyckerelle
+ */
+class FieldsController extends FormController {
+  
     public function fieldupAction(){
     	$id = $this->params()->fromQuery('id', null);
     	$objectManager = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
     	if($id){
     		$customfield = $objectManager->getRepository('Application\Entity\CustomField')->find($id);
     		if($customfield){
-    			//get the field just before
-    			$qb = $objectManager->createQueryBuilder();
-    			$qb->select('f')
-    				->from('Application\Entity\CustomField', 'f')
-    				->andWhere('f.category = '.$customfield->getCategory()->getId())
-    				->andWhere('f.place < '.$customfield->getPlace())
-    				->orderBy('f.place','DESC')
-    				->setMaxResults(1);
-    			$result = $qb->getQuery()->getSingleResult();
-    			//switch places
-    			$temp = $result->getPlace();
-    			$result->setPlace($customfield->getPlace());
-    			$customfield->setPlace($temp);
-    			$objectManager->persist($result);
+    			$customfield->setPlace($customfield->getPlace() - 1);
     			$objectManager->persist($customfield);
     			$objectManager->flush();
     		}
@@ -56,20 +56,7 @@ class FieldsController extends AbstractActionController
     	if($id){
     		$customfield = $objectManager->getRepository('Application\Entity\CustomField')->find($id);
     		if($customfield){
-    			//get the field just after
-    			$qb = $objectManager->createQueryBuilder();
-    			$qb->select('f')
-    			->from('Application\Entity\CustomField', 'f')
-    			->andWhere('f.category = '.$customfield->getCategory()->getId())
-    			->andWhere('f.place > '.$customfield->getPlace())
-    			->orderBy('f.place','ASC')
-    			->setMaxResults(1);
-    			$result = $qb->getQuery()->getSingleResult();
-    			//switch places
-    			$temp = $result->getPlace();
-    			$result->setPlace($customfield->getPlace());
-    			$customfield->setPlace($temp);
-    			$objectManager->persist($result);
+    			$customfield->setPlace($customfield->getPlace() + 1);
     			$objectManager->persist($customfield);
     			$objectManager->flush();
     		}
@@ -122,22 +109,23 @@ class FieldsController extends AbstractActionController
     		
     		if($form->isValid()){
     			$customfield->setCategory($objectManager->getRepository('Application\Entity\Category')->find($post['category']));
-    			$customfield->setType($objectManager->getRepository('Application\Entity\CustomFieldType')->find($post['type']));
-				// calculate next order avalaible
-				$qb = $objectManager->createQueryBuilder ();
-				$qb->select ( 'MAX(f.place)' )
-					->from ( 'Application\Entity\CustomField', 'f' )
-					->where ( 'f.category = ' . $post ['category'] );
-				
-				if(!$id){ //new field : calculate last place
-					$result = $qb->getQuery ()->getSingleResult ();
-					$order = $result[1] + 1;    			
-    				$customfield->setPlace($order);
-				}
-				
-				$objectManager->persist($customfield);
-    			$objectManager->flush();
-    			$this->flashMessenger()->addSuccessMessage("Champ modifié");
+                        if($id){
+                            //modification d'un champ
+                            //si changement de type de champ, il faut vérifier la cohérence pour éviter un crash futur
+                            $fieldtype = $customfield->getType();
+                            if($fieldtype->getId() != $post['type']){
+                                
+                            }
+                        } else {
+                            $customfield->setType($objectManager->getRepository('Application\Entity\CustomFieldType')->find($post['type']));
+                        }
+			$objectManager->persist($customfield);
+                        try{
+                            $objectManager->flush();
+                            $this->flashMessenger()->addSuccessMessage("Champ enregistré");
+                        } catch (\Exception $ex) {
+                            $this->flashMessenger()->addErrorMessage($ex->getMessage());
+                        }
     		} else {
     			$this->flashMessenger()->addErrorMessage("Impossible de modifier le champ.");
     			//traitement des erreurs de validation
@@ -208,21 +196,5 @@ class FieldsController extends AbstractActionController
     	
     	return array('form' => $form, 'customfield'=>$customfield);
     }
-    
-    //TODO Factoriser
-    private function processFormMessages($messages){
-    	foreach($messages as $key => $message){
-    		foreach($message as $mkey => $mvalue){//les messages sont de la forme 'type_message' => 'message'
-    			if(is_array($mvalue)){
-    				foreach ($mvalue as $nkey => $nvalue){//les fieldsets sont un niveau en dessous
-    					$this->flashMessenger()->addErrorMessage(
-    							"Champ ".$mkey." incorrect : ".$nvalue);
-    				}
-    			} else {
-    				$this->flashMessenger()->addErrorMessage(
-    						"Champ ".$key." incorrect : ".$mvalue);
-    			}
-    		}
-    	}
-    }
+
 }
--- module/Application/src/Application/Controller/EventsController.php
+++ module/Application/src/Application/Controller/EventsController.php
@@ -1,7 +1,19 @@
 <?php
-/**
- * Epeires 2
- * @license   https://www.gnu.org/licenses/agpl-3.0.html Affero Gnu Public License
+/*
+ *  This file is part of Epeires².
+ *  Epeires² is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  Epeires² is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with Epeires².  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 namespace Application\Controller;
@@ -23,6 +35,9 @@ use Zend\Form\Element;
 use ZfcRbac\Exception\UnauthorizedException;
 use Application\Entity\FrequencyCategory;
 
+/**
+ * @author Bruno Spyckerelle
+ */
 class EventsController extends ZoneController {
 	
     public function indexAction(){    	
