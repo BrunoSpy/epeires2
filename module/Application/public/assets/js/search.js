15fec6103cc585120f76d757cdce602dec670de0
BrunoSpy <bruno@spyckerelle.fr>
2015-01-31 23:35:47 +0100

    Correction bug affichage resultats recherche

--- module/Application/public/assets/js/search.js
+++ module/Application/public/assets/js/search.js
@@ -48,8 +48,9 @@ var search = function(url){
 		   var container = $("#search-results");
 		   if(!container.is(e.target) && container.has(e.target).length === 0){
 			   if($('#search-results').is(':visible')){
-				   container.slideUp('fast');
-				   $("#search-results").offset({top:0, left:0});
+				   container.slideUp('fast', function(){
+					   $("#search-results").css({'top':'0px', 'left':'0px'});
+				   });
 			   }
 		   }
 	   });
