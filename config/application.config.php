4db5f6f9948b2445d3d1f960ef09a7859b96ebce
BrunoSpy <bruno@spyckerelle.fr>
2015-06-30 21:17:30 +0200

    Premiers essais avec opentbs

--- config/application.config.php
+++ config/application.config.php
@@ -1,47 +1,48 @@
 <?php
-//set APP_ENV = 'development' in httpd.conf or virtualhost conf to set up a dev environment
-$env = getenv('APP_ENV') ?: 'production';
+// set APP_ENV = 'development' in httpd.conf or virtualhost conf to set up a dev environment
+$env = getenv ( 'APP_ENV' ) ?  : 'production';
 
 // Production modules
-$modules = array(
-	'DoctrineModule',
-        'DoctrineORMModule',
-        'DoctrineMigrationsModule',
-        'ZfcBase',
-	'ZfcRbac',
-	'ZfcUser',
-        'ZfcUserDoctrineORM',
-        'AssetManager',
-        'DOMPDFModule',
-	'Core',
-        'Application',
-        'Administration',
-        'IPO',
+$modules = array (
+		'DoctrineModule',
+		'DoctrineORMModule',
+		'DoctrineMigrationsModule',
+		'ZfcBase',
+		'ZfcRbac',
+		'ZfcUser',
+		'ZfcUserDoctrineORM',
+		'AssetManager',
+		'DOMPDFModule',
+		'OpentbsBundle',
+		'Core',
+		'Application',
+		'Administration',
+		'IPO' 
 );
 
-if($env == 'development') {
-	$modules[] = 'ZendDeveloperTools';
+if ($env == 'development') {
+	$modules [] = 'ZendDeveloperTools';
 }
 
-return array(
-    'modules' => $modules,
+return array (
+		'modules' => $modules,
 		
-    'module_listener_options' => array(
-        'module_paths' => array(
-            './module',
-            './vendor'
-            ),
-        'config_glob_paths' => array(
-        	sprintf('config/autoload/{,*.}{global,%s,local}.php', $env)
-        ),
-
-    	//cache only for production
-        'config_cache_enabled' => ($env == 'production'),
-    	'config_cache_key' => 'app_config',
-    	'module_map_cache_enabled' => ($env == 'production'),
-    	'module_map_cache_key' => 'module_map',
-    	'cache_dir' => 'data/cache/',
-    		
-        ),
-		
-    );
+		'module_listener_options' => array (
+				'module_paths' => array (
+						'./module',
+						'./vendor' 
+				),
+				'config_glob_paths' => array (
+						sprintf ( 'config/autoload/{,*.}{global,%s,local}.php', $env ) 
+				),
+				
+				// cache only for production
+				'config_cache_enabled' => ($env == 'production'),
+				'config_cache_key' => 'app_config',
+				'module_map_cache_enabled' => ($env == 'production'),
+				'module_map_cache_key' => 'module_map',
+				'cache_dir' => 'data/cache/' 
+		)
+		 
+)
+;
