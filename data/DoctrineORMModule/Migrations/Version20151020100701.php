47926b3181ec4e710bae040ab63fe593d511e064
Bruno Spyckerelle <bruno.spyckerelle@aviation-civile.gouv.fr>
2015-10-20 11:16:49 +0200

    Fixes #883

---
+++ data/DoctrineORMModule/Migrations/Version20151020100701.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace DoctrineORMModule\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Ajout de la capacité "programmé par défaut" aux modèles.
+ */
+class Version20151020100701 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE PredefinedEvent ADD programmed TINYINT(1) NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE PredefinedEvent DROP programmed');
+    }
+}
