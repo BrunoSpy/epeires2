5dcf9ab0b734b428365bfe77b56f22e2c0672aa5
bruno.spyckerelle <bruno.spyckerelle@aviation-civile.gouv.fr>
2016-08-10 12:05:33 +0200

    Fixes #1507

--- module/Application/public/assets/js/jquery.ui.timeline.js
+++ module/Application/public/assets/js/jquery.ui.timeline.js
@@ -1521,7 +1521,11 @@
          */
         _hideEvent: function (event) {
             var elmt = this.element.find('#event' + event.id);
+            //remove tooltips before
+            elmt.find('span.badge.recurrence').tooltip('destroy');
+            elmt.tooltip('destroy');
             elmt.fadeOut(function () {
+
                 //remove from DOM
                 $(this).remove();
             });
