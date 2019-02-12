639d3d3bcd178cc8fcc1c51fb4ec49cd1b7c3b39
bruno.spyckerelle <bruno.spyckerelle@aviation-civile.gouv.fr>
2019-02-12 16:57:18 +0100

    Compat Mariadb 10.3

--- data/DoctrineORMModule/Migrations/Version20140722101302.php
+++ data/DoctrineORMModule/Migrations/Version20140722101302.php
@@ -15,7 +15,7 @@ class Version20140722101302 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
-        $this->addSql("ALTER TABLE categories ADD system TINYINT(1) NOT NULL");
+        $this->addSql("ALTER TABLE categories ADD `system` TINYINT(1) NOT NULL");
     }
 
     public function down(Schema $schema)
