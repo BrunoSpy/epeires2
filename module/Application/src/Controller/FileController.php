9d938413e32e550c9c173641323cfb175aa7a9a7
bruno.spyckerelle <bruno.spyckerelle@aviation-civile.gouv.fr>
2019-08-21 14:24:22 +0200

    Fixes #25597

--- module/Application/src/Controller/FileController.php
+++ module/Application/src/Controller/FileController.php
@@ -138,4 +138,42 @@ class FileController extends FormController
         
         return $viewmodel;
     }
+
+    private function parseSize($size) {
+        $unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
+        $size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
+        if ($unit) {
+            // Find the position of the unit in the ordered string which is the power of magnitude to multiply a kilobyte by.
+            return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
+        }
+        else {
+            return round($size);
+        }
+    }
+
+    /**
+     * Return post_max_size or upload_max_filesize PHP limits in bytes
+     */
+    public function getmaxsizeAction()
+    {
+        static $max_size = -1;
+
+        if ($max_size < 0) {
+            // Start with post_max_size.
+            $post_max_size = $this->parseSize(ini_get('post_max_size'));
+            if ($post_max_size > 0) {
+                $max_size = $post_max_size;
+            }
+
+            // If upload_max_size is less, then reduce. Except if upload_max_size is
+            // zero, which indicates no limit.
+            $upload_max = $this->parseSize(ini_get('upload_max_filesize'));
+            if ($upload_max > 0 && $upload_max < $max_size) {
+                $max_size = $upload_max;
+            }
+        }
+        $json = array();
+        $json[] = $max_size;
+        return new JsonModel($json);
+    }
 }
