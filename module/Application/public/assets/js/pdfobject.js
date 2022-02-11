c6d7f2d7aa43a9ab65bc464c7c11cb0a7c99da10
Bruno Spyckerelle <bruno@spyckerelle.fr>
2022-02-11 12:00:43 +0100

    Add pdfjs fallback

--- module/Application/public/assets/js/pdfobject.js
+++ module/Application/public/assets/js/pdfobject.js
@@ -1,6 +1,10 @@
 window.open = function (open) {
+
     return function (url, name, features) {
+        let options = {
+            PDFJS_URL: "../components/pdfjs/web/viewer.html"
+        };
         //intercept pdf
-        return PDFObject.embed(url, '#pdfviewer');
+        return PDFObject.embed(url, '#pdfviewer', options);
     };
 }(window.open);
