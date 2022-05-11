5a16e59fa18097710bba622d8954bac69b32fe87
Bruno Spyckerelle <bruno@spyckerelle.fr>
2022-05-11 17:02:41 +0200

    Cleanup migration

--- data/DoctrineORMModule/Migrations/Version20220511130019.php
+++ data/DoctrineORMModule/Migrations/Version20220511130019.php
@@ -23,7 +23,6 @@ final class Version20220511130019 extends AbstractMigration
         $this->addSql('ALTER TABLE categories ADD fieldname2_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE categories ADD CONSTRAINT FK_3AF34668317426DD FOREIGN KEY (fieldname2_id) REFERENCES customfields (id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_3AF34668317426DD ON categories (fieldname2_id)');
-        $this->addSql('ALTER TABLE log CHANGE object_class object_class VARCHAR(191) NOT NULL, CHANGE username username VARCHAR(191) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
@@ -32,6 +31,5 @@ final class Version20220511130019 extends AbstractMigration
         $this->addSql('ALTER TABLE categories DROP FOREIGN KEY FK_3AF34668317426DD');
         $this->addSql('DROP INDEX UNIQ_3AF34668317426DD ON categories');
         $this->addSql('ALTER TABLE categories DROP fieldname2_id');
-        $this->addSql('ALTER TABLE log CHANGE object_class object_class VARCHAR(255) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`, CHANGE username username VARCHAR(255) CHARACTER SET utf8 DEFAULT NULL COLLATE `utf8_unicode_ci`');
     }
 }
